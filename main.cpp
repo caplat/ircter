73b1ecc1d1d8e958ee05934a2babe385e4da3fba
Adrien Gallet <agallet@k0r3p9.42mulhouse.fr>
2024-03-19 16:16:17 +0100

    try to create socket 3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sys/socket.h> /* Tout ce qui est lie aux socket*/
+#include <netinet/in.h> /* sockaddr_in*/
 #include <cstdlib>
 #include <sys/types.h> /* Utilise certaines macros */
 #include <netdb.h> /* gethostbyname*/
@@ -15,11 +16,7 @@ int main(int argc, char** argv)
     int _error = errno;
     std::cout << PF_INET6 << std::endl;
     int _so = socket(PF_INET, SOCK_STREAM, 0);
-    std::cout << "fd socket : " << _so << std::endl;
-
-    addrinfo _addr;
-    std::cout << "addrinfo.ai_family : " <<_addr.ai_family << std::endl;
-    
+    std::cout << "fd socket : " << _so << std::endl; 
     
     sockaddr _serv; 
     _serv.sa_family = PF_INET;
@@ -33,7 +30,11 @@ int main(int argc, char** argv)
     std::cout << "_host->h_length : " <<_host->h_length << std::endl; 
     std::cout << "_PATH_HOSTS : " <<_PATH_HOSTS << std::endl;
 
-    int _named = bind(_so, &_serv, sizeof(_so));
+    sockaddr_in _ser;
+    _ser.sin_port = htons(4242);
+    _ser.sin_family = AF_INET;
+    _ser.sin_addr.s_addr = htonl(INADDR_ANY);
+    int _named = bind(_so, (sockaddr*) &_ser, sizeof(_ser));
     std::cout << "Try to bind socket" << std::endl;
     if (_named == -1)
         std::cout << _error << " : " << std::strerror(errno) << std::endl;
