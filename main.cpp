bdbf5a6c81395a81bf5daea842dfa71b3e8cbbea
Adrien Gallet <agallet@k0r2p2.42mulhouse.fr>
2024-03-22 17:39:05 +0100

    try to create socket 5

--- main.cpp
+++ main.cpp
@@ -8,59 +8,41 @@
 #include <cstring> /*std::strerror*/
 #include <unistd.h> /* */
 
+
+void set_addrinfo(addrinfo **h)
+{
+    addrinfo _init;
+
+    memset(&_init, 0, sizeof _init);
+    _init.ai_family = AF_INET;
+    _init.ai_flags = AI_PASSIVE;
+    _init.ai_socktype = SOCK_STREAM;
+
+    int _test = getaddrinfo(0, "4263", &_init, &(*h));
+    std::cout << _test << std::endl;
+    if (_test != 0)
+    {
+        std::cout << errno << std::strerror(errno) << std::endl;
+        return ;
+    }
+    else
+        std::cout << "Getaddrinfo is ok" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     (void) argc;
     (void) argv;
     
-    int _error = errno;
-    std::cout << PF_INET6 << std::endl;
-    int _so = socket(PF_INET, SOCK_STREAM, 0);
-    std::cout << "fd socket : " << _so << std::endl; 
-    
-    sockaddr _serv; 
-    _serv.sa_family = PF_INET;
-    std::cout << "SOCKADDR" << std::endl;
-    std::cout << "_sockaddr.sa_family : " << _serv.sa_family << std::endl;
-    
-    hostent *_host;
-    _host =  gethostbyname("k0r2p2.42mulhouse.fr");
-    std::cout << "HOST" << std::endl;
-    std::cout << "_host->h_addrtype : " << _host->h_addrtype << std::endl;
-    std::cout << "_host->h_length : " <<_host->h_length << std::endl; 
-    std::cout << "_PATH_HOSTS : " <<_PATH_HOSTS << std::endl;
+   addrinfo *_host;
+   _host = NULL;
+   set_addrinfo(&_host);
 
-    sockaddr_in _ser;
-    _ser.sin_port = htons(4263);
-    _ser.sin_family = AF_INET;
-    _ser.sin_addr.s_addr = htonl(INADDR_ANY);
-    int _named = bind(_so, (sockaddr*) &_ser, sizeof(_ser));
-    std::cout << "Try to bind socket" << std::endl;
-    if (_named == -1)
-        std::cout << _error << " : " << std::strerror(errno) << std::endl;
-    
-    if (listen(_so, 3) == -1)
+    std::cout << _host->ai_protocol << std::endl;
+    int _sock = socket(_host->ai_family, _host->ai_socktype, _host->ai_protocol);
+    if (_sock == -1)
     {
         std::cout << errno << std::strerror(errno) << std::endl;
         return (-1);
     }
-    socklen_t a = sizeof(_ser);
-    int b = accept(_so, (sockaddr*)&_ser, &a);
-    if (b == -1)
-    {
-        std::cout << errno << std::strerror(errno) << std::endl;
-        return (-1);
-    }
-    char _buff[256];
-    int n = read(b, _buff, 256);
-    std::cout << std::string(_buff) << std::endl;
-    if (n < 0)
-    {
-         std::cout << errno << std::strerror(errno) << std::endl;
-        return (-1);       
-    }
-    else
-        write(b, "i'have got your message", 24);
-    close(_so);
-    close(b);
 }
