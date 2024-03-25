fbfaa9a5ac03f0544e44b664679d2d3bad23d442
Adrien Gallet <agallet@k0r2p6.42mulhouse.fr>
2024-03-25 16:59:54 +0100

    try to create socket 6

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 #include <errno.h>
 #include <cstring> /*std::strerror*/
 #include <unistd.h> /* */
-
-
+#include <vector>
+#include <poll.h>
 void set_addrinfo(addrinfo **h)
 {
     addrinfo _init;
-
     memset(&_init, 0, sizeof _init);
     _init.ai_family = AF_INET;
     _init.ai_flags = AI_PASSIVE;
@@ -29,6 +28,14 @@ void set_addrinfo(addrinfo **h)
         std::cout << "Getaddrinfo is ok" << std::endl;
 }
 
+pollfd create_pullfd(int fd, short events)
+{
+    pollfd b;
+    b.fd = fd;
+    b.events = events;
+    return (b);
+}
+
 int main(int argc, char** argv)
 {
     (void) argc;
@@ -38,11 +45,62 @@ int main(int argc, char** argv)
    _host = NULL;
    set_addrinfo(&_host);
 
-    std::cout << _host->ai_protocol << std::endl;
     int _sock = socket(_host->ai_family, _host->ai_socktype, _host->ai_protocol);
     if (_sock == -1)
     {
         std::cout << errno << std::strerror(errno) << std::endl;
         return (-1);
     }
+
+    if (bind(_sock, _host->ai_addr, _host->ai_addrlen) != 0)
+    {
+        std::cout << errno << std::strerror(errno) << std::endl;
+        return (-1);
+    }
+    listen(_sock, 20);
+
+    sockaddr _addr_cli;
+    int _client_fd;
+    socklen_t _size_cli = sizeof(_addr_cli);
+
+    _client_fd = accept(_sock, &_addr_cli, &_size_cli);
+    if (_client_fd == -1)
+    {
+        std::cout << errno << std::strerror(errno) << std::endl;
+        return (-1);
+    }
+    else
+        std::cout << "New connection" << std::endl;
+
+
+    std::vector<pollfd> _poll_fds;
+
+     _poll_fds.push_back(create_pullfd(_sock, POLLIN));
+
+    std::string  _buffer;
+    char _buff_read[100];
+    std::string _content;
+    long _bytes_r = 0;
+    do
+    {
+        _bytes_r = recv(_client_fd, _buff_read, 100, 0);
+        _buff_read[_bytes_r] = 0;
+        if (_bytes_r > 0)
+        {
+            _content.append(_buff_read);
+            bzero(_buff_read, 100);
+            std::cout << _content << std::endl;
+        }
+    }
+    while (_bytes_r > 0);
+    _buffer = "HELLO WORD !";
+    size_t _bytes = 0;
+    size_t _size_buffer = _buffer.size();
+    while (_bytes != _size_buffer) 
+    {
+        _bytes += send(_client_fd, &(_buffer[_bytes]), _size_buffer, 0);
+    }
+    std::cout << _bytes << std::endl;
+    close(_sock);
+    close(_client_fd);
 }
